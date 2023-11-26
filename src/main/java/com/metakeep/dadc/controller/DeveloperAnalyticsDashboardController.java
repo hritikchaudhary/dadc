d2ad68e8de56693d435538735550e7a50374fe21
Hritik Chaudhary <33057454+hritikchaudhary@users.noreply.github.com>
2023-11-26 13:21:25 +0530

    basic api done

--- src/main/java/com/metakeep/dadc/controller/DeveloperAnalyticsDashboardController.java
+++ src/main/java/com/metakeep/dadc/controller/DeveloperAnalyticsDashboardController.java
@@ -1,4 +1,21 @@
 package com.metakeep.dadc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.metakeep.dadc.service.DeveloperAnalyticsDashboardService;
+
+@RestController
 public class DeveloperAnalyticsDashboardController {
+
+    DeveloperAnalyticsDashboardService developerAnalyticsDashboardService;
+    public DeveloperAnalyticsDashboardController(DeveloperAnalyticsDashboardService developerAnalyticsDashboardService) {
+        this.developerAnalyticsDashboardService = developerAnalyticsDashboardService;
+    }
+
+    @GetMapping("/hello")
+    public String hello(String userID){
+        return this.developerAnalyticsDashboardService.helloService(userID);
+    }
 }
--- src/main/java/com/metakeep/dadc/service/DeveloperAnalyticsDashboardService.java
+++ src/main/java/com/metakeep/dadc/service/DeveloperAnalyticsDashboardService.java
@@ -1,4 +1,10 @@
 package com.metakeep.dadc.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class DeveloperAnalyticsDashboardService {
+    public String helloService(String userID){
+        return "Hello, World! " + userID;
+    }
 }
